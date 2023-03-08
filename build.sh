e4905fa43a5c1438e111c1b9c996d5ecf84e8a29
Pedro Henrique <pedrohesm@gmail.com>
2023-03-07 23:44:16 -0300

    adjust in migrate db in production

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -o errexit
 pip install -r requirements.txt
 
 
-python manage.py migrate
+python manage.py migrate --database=production
 python manage.py collectstatic --no-input
 python manage.py loaddata data.json
