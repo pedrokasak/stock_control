c2c979e07678bff1b4a497d141d2d29db440942e
Pedro Henrique <pedrohesm@gmail.com>
2023-03-07 23:48:08 -0300

    adjust settings.py

--- core/settings.py
+++ core/settings.py
@@ -129,7 +129,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates', 'static'), ]
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 FIXTURE_DIRS = (
     os.path.join(BASE_DIR, 'core', 'fixtures'),
