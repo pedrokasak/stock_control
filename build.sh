0d52f63e43d2ca0569771a1b2fb76075ed421629
Pedro Henrique <pedrohesm@gmail.com>
2023-04-28 20:42:21 -0300

    adjust file settings.py statis files

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python manage.py migrate --database=production
-python manage.py collectstatic --no-input
-python manage.py loaddata data.json
+python core/manage.py migrate --database=production
+python core/manage.py collectstatic --no-input
+python core/manage.py loaddata data.json
